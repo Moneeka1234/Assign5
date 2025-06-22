bf9e816d4f8c8e69f0a273b84b510c9ae1e13e58
Moneeka Nehra <122882531+Moneeka1234@users.noreply.github.com>
2025-06-22 20:44:02 +0530

    Update task2.py

--- task2.py
+++ task2.py
@@ -4,9 +4,7 @@ list=[1,2,3,4,5,6,7,8,9,10]
 
 first=list[:5]
 print("Extracted first five elements: ", first)
-print(first)
 
 first.reverse()
 
 print("Reversed extracted elements: ",first)
-print(first)
